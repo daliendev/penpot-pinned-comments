4ab5e3168c892127e71b0cb66ca7421ab1d7f9b0
dalien <daniel.cadeau@proton.me>
2024-11-24 00:29:43 +0000

    feat: poc with store pin in comments & retrieving of matching pinned comments

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,4 +1,4 @@
-import type { Shape } from '@penpot/plugin-types';
+import type { Shape, Comment, CommentThread } from '@penpot/plugin-types';
 import type { PluginMessageEvent } from './model';
 
 penpot.ui.open('Penpot Pinned Comments plugin', `?theme=${penpot.theme}`);
@@ -6,6 +6,10 @@ penpot.ui.open('Penpot Pinned Comments plugin', `?theme=${penpot.theme}`);
 penpot.on('themechange', (theme) => {
   sendMessage({ type: 'theme', content: theme });
 });
+
+penpot.on('selectionchange', (elementIds: string[]) => {
+  pinCommentsToShapes(elementIds)
+})
 
 function sendMessage(message: PluginMessageEvent) {
   penpot.ui.sendMessage(message);
@@ -21,21 +25,24 @@ function isPointInShape(point: { x: number; y: number }, shape: Shape): boolean 
   );
 }
 
-async function pinCommentsToShapes() {
+async function pinCommentsToShapes(shapeIds: string[] = []) {
   const page = penpot.currentPage;
   if (!page) return;
 
   try {
-    const commentThreads = await page.findCommentThreads();
-    const shapes = page.findShapes();
+    const commentThreads: CommentThread[] = await page.findCommentThreads();
+    let shapes = page.findShapes();
+
+    if(shapeIds.length) {
+      shapes = shapes.filter((shape: Shape) => shapeIds.includes(shape.id))
+    }
 
     for (const thread of commentThreads) {
       const commentPosition = thread.position;
       
       const targetShape = shapes.find(shape => isPointInShape(commentPosition, shape));
-      console.log({targetShape, thread})
       if (targetShape) {
-        await thread.reply(`Linked to ${targetShape.name}`)
+        await thread.reply(`📌 Pinned to id:${targetShape.id}`)
       }
     }
   } catch (error) {
@@ -43,5 +50,23 @@ async function pinCommentsToShapes() {
   }
 }
 
-pinCommentsToShapes();
+async function getThreadsByElementId(elementId: string) {
+  const allThreads: CommentThread[]|undefined = await penpot.currentPage?.findCommentThreads();
+  for(const thread of allThreads ?? []) {
+    const comments: Comment[]|undefined = await thread.findComments();
+    let results = [];
+    for (const comment of comments ?? []) {
+      const pattern = /📌 Pinned to id:([a-zA-Z0-9-]+)/;
+      const match = comment.content.match(pattern);
+      if (match) {
+        const extractedId = match[1];
+        if(extractedId === elementId) {
+          results.push(comments.shift()?.content)
+        }
+      }
+    }
+    console.log(results)
+  }
+}
 
+getThreadsByElementId('')
